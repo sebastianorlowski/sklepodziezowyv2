d3ebbb282e13f84f794bdb46c1b9ece471269314
mydarkside <sebekovsky@gmail.com>
2020-01-03 21:21:20 +0100

    Userdao add 1.0

--- src/main/java/dao/UserDaoImpl.java
+++ src/main/java/dao/UserDaoImpl.java
@@ -1,15 +1,36 @@
 package dao;
 
 import api.UserDao;
+import utils.FileUtils;
 import work.User;
 
-import java.io.IOException;
+import java.io.*;
+import java.util.List;
 
 public class UserDaoImpl implements UserDao {
 
-    private String fileName;
+    private String fileName = "User.txt";
+
 
     public void saveUser(User user) throws IOException {
+        List<User> users = getAllUsers();
+        users.add(user);
+        saveUsers(users);
+    }
+
+    public void saveUsers(List<User> users) throws IOException {
+        FileUtils.clearFile(fileName);
+        PrintWriter printWriter = new PrintWriter(new FileOutputStream(fileName, true));
+        for (User user : users) {
+            printWriter.write(user.toString() + "\n");
+        }
+
+        printWriter.close();
+
+    }
+
+
+
 
 
 
